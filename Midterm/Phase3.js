bb38df6ec0a088fd5c7c603afefa7e7f598e5a65
CliffStrong2 <fargopie@yahoo.com>
2025-10-21 03:19:10 -0400

    midterm progress

---
+++ Midterm/Phase3.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400); 
+  noStroke();
+}
+
+function drawObject(x, y, s) {
+  push();
+  translate(x, y);
+  scale(s);
+  fill(0); 
+  rect(40, 10,  60, 10);
+  rect(40, 20, 20, 10);
+  rect(80, 20, 20, 10);
+  stroke('black')
+  strokeWeight(5);
+  line(60, 50, 80, 50);
+  line(40, 40, 60, 50);
+  line(100, 40, 80, 50);
+  ellipse(70, 40, 5, 5);
+  pop();
+}
+
+function draw() {
+  drawObject(0, 0, 1);
+  drawObject(0, 50, 1);
+}
